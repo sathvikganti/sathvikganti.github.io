450773ae3e872f51aa00d763cd7ecb970263620d
Sathvik G <sathvik1105@gmail.com>
2022-04-18 15:29:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 
 $(document).ready(function () {
     $("#arrowright").click(function () {
-        if (document.getElementById("mainbar").textContent == "Intro") {
+        if (document.getElementById("heading").textContent == "Intro") {
             $(".mainbar").load("/projects.html .mainbar");
-        } else if (document.getElementById("mainbar").textContent == "Projects") {
+        } else if (document.getElementById("heading").textContent == "Projects") {
             $(".mainbar").load("/others.html .mainbar");
         } else {
              $(".mainbar").load("/projects.html .mainbar");
         }
     });
     $("#arrowleft").click(function () {
-        if (document.getElementById("mainbar").textContent == "Other") {
+        if (document.getElementById("heading").textContent == "Other") {
             $(".mainbar").load("/projects.html .mainbar");
         } else {
             $(".mainbar").load("/index.html .mainbar");
